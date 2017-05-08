294ac0e12634e5f7803d2d13e07949c94faf0a81
Henry Doan <henrydoan96@gmail.com>
2017-05-08 11:29:05 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var Twit  = require('twit');
+var Cred = require('./credentials.js');
+
+// var bot = new Twit ({
+// 	consumer_key: '';
+// 	consumer_secret: '';
+// 	access_token: '';
+// 	access_token_secret: '';
+// 	timeout_ms: 60*1000
+// })
+
+console.log(`this is a test: ${Cred.key_test} `)
