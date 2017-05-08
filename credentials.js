f87a3b292d9f3537dc5c0691f71f6910b324bb31
Henry Doan <henrydoan96@gmail.com>
2017-05-08 11:36:14 -0600

    this is further testing

--- credentials.js
+++
@@ -1,7 +0,0 @@
-exports.consumer_key = '';
-exports.consumer_secret = '';
-exports.access_token = '';
-exports.access_token_secret = '';
-
-// exports.key_test = 'sfdasldjfh023841843';
-// this is a test
