a1551f81fc91388e3f17ece8a15b54d0bd33e7e8
Henry Doan <henrydoan96@gmail.com>
2017-05-13 15:45:19 -0600

    NASA api with media tweets

--- media_tweets.js
+++ media_tweets.js
@@ -1,5 +1,8 @@
 var Twit  = require('twit');
+var request = require('request');
+var fs = require('fs');
 var Cred = require('./credentials.js');
+var NasaKey = require('./nasa_credentials.js');
 
 var bot = new Twit ({
 	consumer_key: `${Cred.consumer_key}`,
@@ -10,13 +13,78 @@ var bot = new Twit ({
 })
 
 /* 
-	The Steam api gives you an access of real time tweets.
+	The NASA api gives you access to photos and videos of the nasa
+	This function in particular will return you the photo of the day
+	and post it on your twitter account.
 
-	There are two ways to do this, the first is the sample of public 
-	tweets from the last 7 days.
-
-	The code block below will give you a lot of data, so be sure to
-	hit control c to stop the data flow.
+	Make sure to sign up and use the NASA API Here:
+		https://api.nasa.gov/index.html#apply-for-an-api-key
+	and fill out the nasa_credentials.js same as the example.
 	
 */
 
+function getPhoto(){
+	var parameters = {
+		url:'https://api.nasa.gov/planetary/apod',
+		qs: {
+			api_key: NasaKey.nasa_API_key
+		},
+		encoding: 'binary'
+	};
+
+	request.get(parameters, function(err, response, body) {
+		body = JSON.parse(body);
+		saveFile(body, 'nasa.jpg');
+	});
+}
+
+	// Saves the photo in this directory
+function saveFile(body, fileName) {
+	var file = fs.createWriteStream(fileName);
+	request(body).pipe(file).on('close', function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Media Saved.');
+			// shows detais of the photo
+			// console.log(body);
+			var descriptionText = body.title;
+			uploadMedia(descriptionText, fileName);
+		}
+	})
+}
+
+// uploads it to twitter
+function uploadMedia(descriptionText, fileName) {
+	var filePath = __dirname + '/' + fileName;
+	bot.postMediaChunked({file_path: filePath},
+		function(err, data, response) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(data);
+				var params = {
+					status: descriptionText,
+					media_ids: data.media_id_string
+				};
+				postStatus(params);
+			}
+		});
+}
+
+// post it to twitter
+function postStatus(params) {
+	bot.post('statuses/update', params, function(err, data, response) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('Status Posted!');
+		}
+	});
+}
+
+// Gives youthe Photo from the NASA API
+getPhoto();
+
+// Gives you the video from the NASA API
+uploadMedia('Video from NASA', 'nasa_video.mp4')
